8ff595eeb7ba10a6e8ae4c28ce86c2bc357820bc
Tamás Major <majortamas313@gmail.com>
2024-05-24 16:43:12 +0200

    adding safeareaview

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { StyleSheet, ImageBackground } from 'react-native';
+import { StyleSheet, ImageBackground, SafeAreaView } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient'
 
 import StartGameScreen from './screens/StartGameScreen';
@@ -21,7 +21,9 @@ export default function App() {
   return (
     <LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.rootScreen}>
       <ImageBackground source={require('./assets/images/background.png')} resizeMode='cover' style={styles.rootScreen} imageStyle={styles.backgroundImage}>
-        {screen}
+        <SafeAreaView style={styles.rootScreen}>
+          {screen}
+        </SafeAreaView>
       </ImageBackground>
     </LinearGradient>
   );
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,9 +1,25 @@
-import { Text } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 function GameScreen() {
     return(
-        <Text>GameScreen</Text>
+        <View style={styles.screen}>
+            <Text>Opponents guess</Text>
+
+            <View>
+                <Text>Higher or lower?</Text>
+            </View>
+            <View>
+
+            </View>
+        </View>
     )
 }
 
 export default GameScreen;
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 24
+    }
+})
